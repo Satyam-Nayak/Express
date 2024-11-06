ac830820d0a4b3c0997de26da73d05f9eac511a8
Satyam-Nayak <satyamn120@gmail.com>
2024-11-06 15:37:58 +0530

    1st class express how to create form using exoress

--- 1st class Express/app.js
+++ 1st class Express/app.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express();
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
-    res.send('Hello Server')
+    res.send(`<fieldset>
+        <form action="/login" method="post">
+            <label for="email">email:</label>
+            <input type="email" id="email" name="email" required><br>
+            <label for="pswd">Password:</label>
+            <input type="password" id="pswd" name="pswd"><br>
+            <button type="submit">submit</button>
+        </form>
+    </fieldset>`)
 })
-
-app.get('/about', (req,res)=>{
-    res.send('This is about page server')
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+    res.send('login successful')
 })
 
 app.listen(3001,err=>{
     if(err) throw err;
-    console.log('Server is running on port 3001')
+    console.log('Server is running on port 3001 http://localhost:3001/ ')
 })
 // how to create a server in express?
