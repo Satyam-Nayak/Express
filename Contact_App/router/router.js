6107403fef117e8d5e43c9ef14c23f7f1b55a477
Satyam-Nayak <satyamn120@gmail.com>
2024-11-21 15:19:58 +0530

    express js

--- Contact_App/router/router.js
+++ Contact_App/router/router.js
@@ -2,6 +2,16 @@ const {Router} = require('express')
 const router = Router()
 const Cnt_Schema=require('../schema/schema')
 
+
+const fs = require('fs')
+router.get('/style',(req,res)=>{
+    fs.readFile('./public/addContact.css',(err,data)=>{
+        if(err) throw err;
+        res.end(data)
+    })
+})
+
+
 router.get('/addContact',(req,res)=>{
     res.render('contact_App/addContact',{title:'Add_Contact'})
 })
