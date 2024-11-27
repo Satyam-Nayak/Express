e259607e524b3f795dcfbfb3d2005b3f49650e8a
Satyam-Nayak <satyamn120@gmail.com>
2024-11-27 16:07:10 +0530

    27 nov 2024 post app

---
+++ Post App/schema.js
@@ -0,0 +1,20 @@
+const {model,Schema} = require(mongoose)
+let postSchema=new Schema({
+    post:{
+        type:String,
+        default: "https://cdn.pixabay.com/photo/2023/09/22/03/51/beautiful-8267949_1280.jpg"
+    },
+    posted_by:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required: true,
+    }
+    
+},{
+    timestamp:true
+})
+
+module.exports= model('post',postSchema)
---
+++ Post App/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT
+const {connectDb} = require('./config/db')
+connectDb()
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express Server!');
+});
+
+app.listen(PORT, err => {
+    if (err) throw err;
+  console.log('Server is running on 5000 "http://localhost:5000/"');
+});
